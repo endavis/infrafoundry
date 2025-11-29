b17724ff9706473dee251bfd0cbcce6266ebb472
Eric Davis <6662995+endavis@users.noreply.github.com>
2025-11-29 11:29:19 +0000

    fix: Make SopsSecretProvider binary check optional for CI (mypy compliant)

--- src/infrafoundry/core/credential_loader/credential_loader.py
+++ src/infrafoundry/core/credential_loader/credential_loader.py
@@ -78,7 +78,12 @@ class CredentialLoader:
             self.config_dir = Path(config_dir)
 
         self._debug_mode = os.getenv("INFRAFOUNDRY_LOG_LEVEL") == "DEBUG"
-        self.secret_provider = secret_provider or SopsSecretProvider()
+
+        skip_sops_checks = os.getenv("INFRAFOUNDRY_SKIP_SOPS_CHECK")
+        force_sops_checks = os.getenv("INFRAFOUNDRY_FORCE_SOPS_CHECK")
+        should_check_sops = bool(force_sops_checks) or not bool(skip_sops_checks)
+
+        self.secret_provider = secret_provider or SopsSecretProvider(check_binary=should_check_sops)
 
     def get_secrets_dir(self, env_name: str) -> Path:
         """Get the environment directory containing secrets.
